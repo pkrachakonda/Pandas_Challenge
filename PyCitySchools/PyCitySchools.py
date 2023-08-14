968e16a9e170086d130b30683a132a5c3ae081cf
pkrachakonda <praveenkumar.rachakonda@gmail.com>
2023-08-14 23:27:50 +0800

    Added files and completed Jupyter Lab Notebook to Github

--- PyCitySchools/PyCitySchools.py
+++ PyCitySchools/PyCitySchools.py
@@ -127,3 +127,88 @@ Reading_Score_Year_df
 
 #___________________________________________________________________________
 
+spending_bins = [0, 585, 630, 645, 680]
+labels = ["<$585", "$585-630", "$630-645", "$645-680"]
+
+School_Summary_df['Average Maths Score'] = School_Summary_df['Average Maths Score'].astype("float64", copy=None, errors='raise')
+School_Summary_df['Average Reading Score'] = School_Summary_df['Average Reading Score'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Passing Maths'] = School_Summary_df['% Passing Maths'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Passing Reading'] = School_Summary_df['% Passing Reading'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Overall Passing'] = School_Summary_df['% Overall Passing'].astype("float64", copy=None, errors='raise')
+
+School_Summary_df["Spending Ranges (Per Student)"] = pd.cut(SS_Per_Student_Budget, bins = spending_bins, labels=labels)
+spending_math_scores = School_Summary_df.groupby(["Spending Ranges (Per Student)"])["Average Maths Score"].mean()
+spending_reading_scores = School_Summary_df.groupby(["Spending Ranges (Per Student)"])["Average Reading Score"].mean()
+spending_passing_math = School_Summary_df.groupby(["Spending Ranges (Per Student)"])["% Passing Maths"].mean()
+spending_passing_reading = School_Summary_df.groupby(["Spending Ranges (Per Student)"])["% Passing Reading"].mean()
+overall_passing_spending = School_Summary_df.groupby(["Spending Ranges (Per Student)"])["% Overall Passing"].mean()
+
+School_Spending_df= pd.DataFrame({"Average Maths Score":spending_math_scores, "Average Reading Score": spending_reading_scores, "% Passing Maths": spending_passing_math,
+                                  "% Passing Reading": spending_passing_reading, "% Overall Passing":overall_passing_spending}, index = labels)
+
+School_Spending_df.index.name = 'Spending Ranges (Per Student)'
+School_Spending_df['Average Maths Score'] = School_Spending_df['Average Maths Score'].map('{:.2f}'.format)
+School_Spending_df['Average Reading Score'] = School_Spending_df['Average Reading Score'].map('{:.2f}'.format)
+School_Spending_df['% Passing Maths'] = School_Spending_df['% Passing Maths'].map('{:.2f}'.format)
+School_Spending_df['% Passing Reading'] = School_Spending_df['% Passing Reading'].map('{:.2f}'.format)
+School_Spending_df['% Overall Passing'] = School_Spending_df['% Overall Passing'].map('{:.2f}'.format)
+
+School_Spending_df
+
+#_____________________________________________________________________________________________________________________
+
+school_size = [0, 1000, 2000, 5000]
+labels = ["Small(<1000)", "Medium(1000-2000)", "Large(2000-5000)"]
+
+School_Summary_df['Average Maths Score'] = School_Summary_df['Average Maths Score'].astype("float64", copy=None, errors='raise')
+School_Summary_df['Average Reading Score'] = School_Summary_df['Average Reading Score'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Passing Maths'] = School_Summary_df['% Passing Maths'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Passing Reading'] = School_Summary_df['% Passing Reading'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Overall Passing'] = School_Summary_df['% Overall Passing'].astype("float64", copy=None, errors='raise')
+
+School_Summary_df["Score by School size"] = pd.cut(SS_Total_students, bins = school_size, labels=labels)
+school_size_math_scores = School_Summary_df.groupby(["Score by School size"])["Average Maths Score"].mean()
+school_size_reading_scores = School_Summary_df.groupby(["Score by School size"])["Average Reading Score"].mean()
+school_size_passing_maths = School_Summary_df.groupby(["Score by School size"])["% Passing Maths"].mean()
+school_size_passing_reading = School_Summary_df.groupby(["Score by School size"])["% Passing Reading"].mean()
+school_size_overall_passing = School_Summary_df.groupby(["Score by School size"])["% Overall Passing"].mean()
+
+Scores_School_Size_df = pd.DataFrame({"Average Maths Score":school_size_math_scores, "Average Reading Score": school_size_reading_scores, "% Passing Maths": school_size_passing_maths,
+                                  "% Passing Reading": school_size_passing_reading, "% Overall Passing":school_size_overall_passing}, index = labels)
+
+Scores_School_Size_df.index.name = 'School Size'
+Scores_School_Size_df['Average Maths Score'] = Scores_School_Size_df['Average Maths Score'].map('{:.6f}'.format)
+Scores_School_Size_df['Average Reading Score'] = Scores_School_Size_df['Average Reading Score'].map('{:.6f}'.format)
+Scores_School_Size_df['% Passing Maths'] = Scores_School_Size_df['% Passing Maths'].map('{:.6f}'.format)
+Scores_School_Size_df['% Passing Reading'] = Scores_School_Size_df['% Passing Reading'].map('{:.6f}'.format)
+Scores_School_Size_df['% Overall Passing'] = Scores_School_Size_df['% Overall Passing'].map('{:.6f}'.format)
+
+Scores_School_Size_df
+#______________________________________________________________________________________________________________________
+
+labels = School_Summary_df["School Type"].unique()
+School_Summary_df['Average Maths Score'] = School_Summary_df['Average Maths Score'].astype("float64", copy=None, errors='raise')
+School_Summary_df['Average Reading Score'] = School_Summary_df['Average Reading Score'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Passing Maths'] = School_Summary_df['% Passing Maths'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Passing Reading'] = School_Summary_df['% Passing Reading'].astype("float64", copy=None, errors='raise')
+School_Summary_df['% Overall Passing'] = School_Summary_df['% Overall Passing'].astype("float64", copy=None, errors='raise')
+
+school_type_math_scores = School_Summary_df.groupby(["School Type"])["Average Maths Score"].mean()
+school_type_reading_scores = School_Summary_df.groupby(["School Type"])["Average Reading Score"].mean()
+school_type_passing_maths = School_Summary_df.groupby(["School Type"])["% Passing Maths"].mean()
+school_type_passing_reading = School_Summary_df.groupby(["School Type"])["% Passing Reading"].mean()
+school_type_overall_passing = School_Summary_df.groupby(["School Type"])["% Overall Passing"].mean()
+
+Scores_School_Type_df = pd.DataFrame({"Average Maths Score":school_type_math_scores, "Average Reading Score": school_type_reading_scores, "% Passing Maths": school_type_passing_maths,
+                                  "% Passing Reading": school_type_passing_reading, "% Overall Passing":school_type_overall_passing}, index = labels)
+
+Scores_School_Type_df.index.name = 'School Type'
+Scores_School_Type_df['Average Maths Score'] = Scores_School_Type_df['Average Maths Score'].map('{:.6f}'.format)
+Scores_School_Type_df['Average Reading Score'] = Scores_School_Type_df['Average Reading Score'].map('{:.6f}'.format)
+Scores_School_Type_df['% Passing Maths'] = Scores_School_Type_df['% Passing Maths'].map('{:.6f}'.format)
+Scores_School_Type_df['% Passing Reading'] = Scores_School_Type_df['% Passing Reading'].map('{:.6f}'.format)
+Scores_School_Type_df['% Overall Passing'] = Scores_School_Type_df['% Overall Passing'].map('{:.6f}'.format)
+
+Scores_School_Type_df
+
+#_________________________________________________________________________________________________________________________
